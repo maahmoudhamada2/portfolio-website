d375e2b843ed901e75da2a0928055013ecf14942
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-01-19 10:42:52 +0200

    feat: Add mobile Footer component for small screens

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,15 @@
+import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import HeroSection from '../HeroSection/HeroSection'
+import Projects from '../Projects/Projects'
 
 export default function App() {
     return (
         <>
             <Header />
+            <HeroSection />
+            <Projects />
+            <Footer />
         </>
     )
 }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,32 @@
+
+import { StyleSheet, css } from "aphrodite/no-important";
+import Socials from "../Socials/Socials";
+
+const footerStyle = StyleSheet.create({
+    footerContainer: {
+        '@media (max-width: 481px)': {
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            backgroundColor: 'rgba(6, 7, 9, 0.99)',
+            color: 'white',
+            gap: '1rem',
+            padding: '1rem'
+        }
+    },
+    copyRight: {
+        '@media (max-width: 481px)': {
+            fontSize: '0.9rem',
+            color: 'gray'
+        }
+    }
+})
+export default function Footer() {
+    return (
+        <footer className={css(footerStyle.footerContainer)}>
+            <Socials />
+            <p className={css(footerStyle.copyRight)}>© 2024 Mahmoud Hamada. All rights reserved.</p>
+        </footer>
+    )
+}
