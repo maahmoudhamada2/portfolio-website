9069102dfdb87f60ed4e74af5993e8ab5728c42d
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-01-20 09:26:18 +0200

    test: Add tests for Header, NavMenu, and NavItems components

---
+++ src/Header/Header.test.js
@@ -0,0 +1,36 @@
+import { render, screen, within } from '@testing-library/react';
+import Header from './Header';
+import * as NavMenu from './NavMenu';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+StyleSheetTestUtils.suppressStyleInjection();
+
+const spyOnNavMenu = jest.spyOn(NavMenu, 'default');
+
+describe('Header Component test suite', () => {
+    afterAll(() => {
+        spyOnNavMenu.mockRestore()
+    })
+    
+    it('Test 1 - Check rendering without crashing', () => {
+        render(<Header />);
+    })
+
+    it('Test 2 - Check Component contain correct html element', () => {
+        render(<Header />);
+
+        // Check for header element
+        const header = screen.getByRole('banner');
+        expect(header).toBeInTheDocument();
+        expect(header).toHaveClass(/headerContainer/)
+
+        // check for website logo
+        expect(within(header).getByRole('img')).toBeInTheDocument()
+        expect(within(header).getByRole('img')).toHaveClass(/logo/)
+        expect(within(header).getByRole('img')).toHaveAttribute('alt', 'ws-logo')
+    })
+    it('Test 3 - Check component renders child component correctly', () => {
+        render(<Header />)
+        expect(spyOnNavMenu).toHaveBeenCalled()
+    })
+})
---
+++ src/Header/NavItems.test.js
@@ -0,0 +1,52 @@
+import { fireEvent, render, screen} from '@testing-library/react';
+import NavItems from './NavItems';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+StyleSheetTestUtils.suppressStyleInjection();
+const toggleMock = jest.fn();
+
+describe('NavItems Component test suites', () => {
+    it('Test 1 - Check component renders without crashing', () => {
+        render(<NavItems />);
+    })
+    it('Test 2 - Check component return correct HTML elements', () => {
+        render(<NavItems />);
+
+        // Get HTML element <li> & <a>
+        const listItems = screen.getAllByRole('listitem');
+        const anchors = screen.getAllByRole('link')
+
+        // Checking Number of <li> & <a> returned === 5
+        expect(listItems).toHaveLength(5);
+        expect(anchors).toHaveLength(5)
+
+        // Checking the first 2 <li>  contain <a> tags
+        expect(listItems[0]).toContainElement(anchors[0]);
+        expect(listItems[1]).toContainElement(anchors[1]);
+
+        // Checking the first 2 <a> text and url
+        expect(anchors[0]).toHaveTextContent('Home')
+        expect(anchors[0]).toHaveAttribute('href', '#herosection')
+
+        expect(anchors[1]).toHaveTextContent('Projects');
+        expect(anchors[1]).toHaveAttribute('href', '#projects')
+
+        // Checking first 2 <li> CSS classes
+        expect(listItems[0]).toHaveClass(/^listItem_.+/);
+        expect(listItems[1]).toHaveClass(/^listItem_.+/);
+
+        // Checking first 2 <a> CSS classes
+        expect(anchors[0]).toHaveClass(/^anchors_.+/);
+        expect(anchors[1]).toHaveClass(/^anchors_.+/);
+    });
+    it("Test 3 - Check Component's prop", () => {
+        render(<NavItems toggleMenu={toggleMock} />);
+
+        // Get all anchors <a>
+        const anchors = screen.getAllByRole('link');
+
+        // Click on the first <a> element and check for toggleMenu
+        fireEvent.click(anchors[0])
+        expect(toggleMock).toHaveBeenCalledTimes(1)
+    })
+})
