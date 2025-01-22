94504204725df2df41e827f7dfa0f296cd7f945b
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-01-22 21:40:52 +0200

    feat: Build Projects component for laptop screens

--- src/Projects/Cards.js
+++ src/Projects/Cards.js
@@ -4,63 +4,84 @@ import { GithubIcon } from "../Socials/SocialSvg";
 const cardStyle = StyleSheet.create({
     articleContainer: {
         // Mobile & Tablet screens width
-        '@media (min-width: 320px) and (max-width: 786px)': {
-            width: '100%',
+        '@media (min-width: 320px) and (max-width: 1024px)': {
             padding: '1rem',
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
             boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.7)',
             borderRadius: '20px',
-            transition: 'all 0.5s'
+            transition: 'all 0.5s',
+            position: 'relative'
+        },
+        // Mobile screen width
+        '@media (min-width: 320px) and (max-width: 481px)': {
+            width: '100%'
+        },
+        // Tablet (small screen width)
+        '@media (min-width: 482px) and (max-width: 546px)': {
+            width: '70vw'
         },
         // Tabelt screen width
-        '@media (min-width: 481px) and (max-width: 786px)': {
+        '@media (min-width: 547px) and (max-width: 786px)': {
             width: 'calc(3rem + 60vw)'
+        },
+        // Laptop (small) screen width
+        '@media (min-width: 786px) and (max-width: 1024px)': {
+            width: '47%'
         }
     },
     cardCover: {
         // Mobile & Tablet screens width
-        '@media (min-width: 320px) and (max-width: 786px)': {
+        '@media (min-width: 320px) and (max-width: 1024px)': {
             width: '100%',
             borderRadius: '20px 20px 0 0'
+        },
+        // Laptop (small) screen width
+        '@media (min-width: 786px) and (max-width: 1024px)': {
+            height: 'calc(1rem + 30vh)'
         }
     },
     cardContent: {
         // Mobile & Tablet screens width
-        '@media (min-width: 320px) and (max-width: 786px)': {
+        '@media (min-width: 320px) and (max-width: 1024px)': {
             display: 'flex',
             flexDirection: 'column',
             padding: '1rem',
             gap: '1rem',
             textAlign: 'center',
-            fontWeight: 'bold'
+            fontWeight: 'bold',
+            marginBottom: '2rem'
         }
     },
     projectCategory: {
-        '@media (min-width: 320px) and (max-width: 786px)': {
+        '@media (min-width: 320px) and (max-width: 1024px)': {
             fontSize: 'calc(0.5rem + 2vw)',
             color: 'red',
         },
         // Tabelt screen width
-        '@media (min-width: 481px) and (max-width: 786px)': {
+        '@media (min-width: 481px) and (max-width: 1024px)': {
             fontSize: 'calc(0.5rem + 1vw)',
         }
     },
     projectTitle: {
         // Mobile & Tabelt screens width
-        '@media (min-width: 320px) and (max-width: 786px)': {
+        '@media (min-width: 320px) and (max-width: 1024px)': {
             // fontSize: '2rem',
             fontSize: 'calc(1rem + 3.5vw)'
         },
         // Tabelt screen width
-        '@media (min-width: 481px) and (max-width: 786px)': {
+        '@media (min-width: 481px) and (max-width: 1024px)': {
             fontSize: 'calc(1rem + 2.3vw)'
+        },
+        // Laptop (small) screen width
+        '@media (min-width: 786px) and (max-width: 1024px)': {
+            fontSize: 'calc(1rem + 2vw)'
         }
     },
     projectIntro: {
         // Mobile & Tabelt screens width
-        '@media (min-width: 320px) and (max-width: 786px)': {
+        '@media (min-width: 320px) and (max-width: 1024px)': {
             color: 'gray',
             lineHeight: '1.5rem',
             fontFamily: 'lora-regular, serif',
@@ -68,20 +89,28 @@ const cardStyle = StyleSheet.create({
             fontSize: 'calc(0.5rem + 2vw)'
         },
         // Tabelt screen width
-        '@media (min-width: 481px) and (max-width: 786px)': {
+        '@media (min-width: 481px) and (max-width: 1024px)': {
             fontSize: 'calc(0.7rem + 1vw)',
             lineHeight: '1.7rem'
+        },
+        // Laptop (small) screen width
+        '@media (min-width: 786px) and (max-width: 1024px)': {
+            fontSize: 'calc(0.5rem + 1vw)',
+            lineHeight: '1.5rem'
         }
     },
     codeBtn: {
         // Mobile & Tabelt screens width
-        '@media (min-width: 320px) and (max-width: 786px)': {
+        '@media (min-width: 320px) and (max-width: 1024px)': {
             width: '45px',
             height: '45px',
             border: '0',
             borderRadius: '5px',
             backgroundColor: 'black',
-            marginTop: '1.5rem'
+        },
+        // Laptop (small) screen width
+        '@media (min-width: 786px) and (max-width: 1024px)': {
+            padding: '0.5rem'
         }
     }
 })
