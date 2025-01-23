c77e7255296f1ede4e73f761492145e003efeaf6
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-01-23 18:07:36 +0200

    feat: Build Footer component for desktop screens

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,26 +3,21 @@ import Socials from "../Socials/Socials";
 
 const footerStyle = StyleSheet.create({
     footerContainer: {
-        // Mobile, Tablet & Laptop (small) screens width
-        '@media (min-width: 320px) and (max-width: 1024px)': {
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            backgroundColor: 'rgba(6, 7, 9, 0.99)',
-            color: 'white',
-            gap: '1rem',
-            padding: '1rem',
-            transition: 'all 0.5s'
-        }
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: 'rgba(6, 7, 9, 0.99)',
+        color: 'white',
+        gap: '1rem',
+        padding: '1rem',
+        transition: 'all 0.5s'
     },
     copyRight: {
-        // Mobile, Tablet & Laptop (small) screens width
-        '@media (min-width: 320px) and (max-width: 1024px)': {
-            fontSize: '0.9rem',
-            color: 'gray',
-            textAlign: 'center'
-        }
+        fontSize: '0.9rem',
+        color: 'gray',
+        textAlign: 'center',
+        marginTop: '0.6rem'
     }
 })
 export default function Footer() {
