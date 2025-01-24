d39128554f3433192990bf4dc6ac991699f25ea7
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-01-24 08:00:09 +0200

    feat: Build ContactMe component for desktop screens

--- src/ContactMe/ContactInfo.js
+++ src/ContactMe/ContactInfo.js
@@ -3,69 +3,68 @@ import { LocationPinIcon, MailIcon, PhoneCallIcon } from "./ContactInfoIcons"
 
 const contactInfoStyle = StyleSheet.create({
     sectionContainer: {
-
-        // Mobile, Tablet & Laptop (small) screens width
-        '@media (min-width: 320px) and (max-width: 1024px)': {
-            width: '100%',
-            height: '510px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-            margin: '1rem 0',
-            gap: '4rem'
-        },
+        // Shared CSS styles for all sizes
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        margin: '3rem 0',
+        gap: '4rem',
         // Laptop (small) screen width
         '@media (min-width: 786px) and (max-width: 1024px)': {
             flexDirection: 'row',
             flexWrap: 'wrap',
-            gap: '0'
+            gap: '5rem 0rem'
+        },
+        // Desktop screen width 
+        '@media (min-width: 1024px) and (max-width: 1200px)': {
+            flexDirection: 'row',
+            flexWrap: 'nowrap',
+            gap: '1rem'
         }
     },
     articleContainer: {
-        // Mobile, Tablet & Laptop (small) screens width
-        '@media (min-width: 320px) and (max-width: 1024px)': {
-            width: '100%',
-            height: '150px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
+        width: '100%',
+        height: '150px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+
         // Laptop (small) screen width
-        '@media (min-width: 786px) and (max-width: 1024px)': {
+        '@media (min-width: 786px) and (max-width: 1200px)': {
             width: '50%',
-            height: '300px'
-        }
+        },
     },
     contactInfoHeading: {
-        // Mobile, Tablet & Laptop (small) screens width
-        '@media (min-width: 320px) and (max-width: 1024px)': {
+        fontSize: 'calc(1rem + 2vw)',
+        fontWeight: 'bold',
+        marginTop: 'calc(1rem + 1vh)',
+        marginBottom: 'calc(0.5rem + 1vh)',
+        color: 'red',
+        letterSpacing: '0.2rem',
+
+        // Desktop screen width 
+        '@media (min-width: 1024px) and (max-width: 1200px)': {
             fontSize: 'calc(1rem + 1vw)',
-            fontWeight: 'bold',
-            marginTop: 'calc(1rem + 1vh)',
-            marginBottom: 'calc(0.5rem + 1vh)',
-            color: 'red',
-            letterSpacing: '0.2rem'
+            marginBottom: 'calc(1rem + 0.5vh)'
         }
     },
     paragraphColor: {
-        // Mobile, Tablet & Laptop (small) screens width
-        '@media (min-width: 320px) and (max-width: 1024px)': {
-            color: 'gray'
-        }
+        fontSize: '1.2rem',
+        color: 'gray'
     }
 });
 
 export default function ContactInfo() {
     return (
         <section className={css(contactInfoStyle.sectionContainer)}>
+
             <article className={css(contactInfoStyle.articleContainer)}>
-                <LocationPinIcon />
-                <h3 className={css(contactInfoStyle.contactInfoHeading)}>WHERE TO FIND ME</h3>
-                <address>
-                    <p className={css(contactInfoStyle.paragraphColor)}>Alexnadira, Egypt</p>
-                </address>
+                <MailIcon />
+                <h3 className={css(contactInfoStyle.contactInfoHeading)}>EMAIL ME AT</h3>
+                <p className={css(contactInfoStyle.paragraphColor)}>maahmoud.hamada@hotmail.com</p>
             </article>
             <article className={css(contactInfoStyle.articleContainer)}>
                 <PhoneCallIcon />
@@ -73,9 +72,11 @@ export default function ContactInfo() {
                 <p className={css(contactInfoStyle.paragraphColor)}>0123456789</p>
             </article>
             <article className={css(contactInfoStyle.articleContainer)}>
-                <MailIcon />
-                <h3 className={css(contactInfoStyle.contactInfoHeading)}>EMAIL ME AT</h3>
-                <p className={css(contactInfoStyle.paragraphColor)}>maahmoud.hamada@hotmail.com</p>
+                <LocationPinIcon />
+                <h3 className={css(contactInfoStyle.contactInfoHeading)}>WHERE TO FIND ME</h3>
+                <address>
+                    <p className={css(contactInfoStyle.paragraphColor)}>Alexnadira, Egypt</p>
+                </address>
             </article>
         </section>
     )
